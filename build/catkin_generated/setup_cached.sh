7915f88a158be4c03707ed62f4dc1c20d7b62ed7
Alex <alex_halle@hotmail.com>
2016-12-13 12:47:20 +0000

    Initialised Repo

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/alex/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/opt/ros/kinetic/lib:/home/alex/Code/VulkanSDK/1.0.30.0/x86_64/lib"
+export PWD="/home/alex/catkin_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/alex/catkin_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/alex/catkin_ws/src:$ROS_PACKAGE_PATH"
